0fdd60d2dafddce3b7a79bd810294ca86cf99074
KangRok Yoon <ykr0919@naver.com>
2024-01-08 16:38:48 +0900

    Study-5 sencond commit

--- SwiftStudy-5/SwiftStudy-5/ViewController.swift
+++ SwiftStudy-5/SwiftStudy-5/ViewController.swift
@@ -2,18 +2,50 @@
 //  ViewController.swift
 //  SwiftStudy-5
 //
-//  Created by 윤강록 on 1/7/24.
+//  Created by 윤강록 on 1/8/24.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var imageBox: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textField.delegate = self
+       
+        setup()
     }
-
-
+    //MARK: setup
+    func setup() {
+        textField.placeholder = "Enter the name"
+        textField.backgroundColor = .lightGray
+        label.text = "WIKIFACE"
+    }
 }
 
+//MARK: UITextFieldDelegate
+extension ViewController: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        guard let oldString = textField.text, let newRange = Range(range, in: oldString) else {return true}
+        
+        let inputString = string.trimmingCharacters(in: .whitespacesAndNewlines)
+                let newString = oldString.replacingCharacters(in: newRange, with: inputString)
+                    .trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if let image = UIImage(named: newString) {
+            imageBox.image = image
+        } else {
+            imageBox.image = nil
+        }
+            return true
+    }
+}
